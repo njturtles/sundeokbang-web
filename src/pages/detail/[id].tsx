8285877f65ede2e2ff8a35d1e2ba753ee91fb6a0
용원 <auau3@kakao.com>
2024-06-01 15:20:36 +0900

    feature: detail page data fetching 방식 변경 - react-query hooks -> getServerSideProps - getServerSideProps에선 hook 사용 불가, react-query 제거

--- src/pages/detail/[id].tsx
+++ src/pages/detail/[id].tsx
@@ -14,28 +14,33 @@ import { useQuery } from "@tanstack/react-query";
 import { mockApi } from "@/apis/mock";
 import { ResponseType } from "@/types/response";
 import ImageDetail from "@/components/detail/ImageDetail";
+import {
+    GetServerSideProps,
+    GetServerSidePropsContext,
+    InferGetServerSidePropsType,
+} from "next";
 
 const apiKey = `${process.env.NEXT_PUBLIC_NAVERMAP_API_KEY_STATIC}`;
 const apiSecret = `${process.env.NEXT_PUBLIC_NAVERMAP_API_SECRET_STATIC}`;
 
-const Detail = () => {
+export const getServerSideProps = (async ({
+    query,
+}: GetServerSidePropsContext) => {
+    if (!query.id) return { notFound: true };
+    const response = await mockApi
+        .getRoomById(String(query.id))
+        .then(({ data }) => data)
+        .catch((e) => {
+            throw new Error(`An error occured while fetching data.: ${e}`);
+        });
+    if (response.code === 4040) return { notFound: true };
+    return { props: { data: response } };
+}) satisfies GetServerSideProps;
+
+const Detail = ({
+    data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
     const [olVisible, setOlVisible] = useState<boolean>(false);
-    const router = useRouter();
-    const roomId = router.query.id;
-    const { isFetching, isError, data } = useQuery({
-        enabled: roomId !== undefined,
-        queryKey: ["room_detail"],
-        queryFn: () =>
-            mockApi
-                .getRoomById(String(roomId || ""))
-                .then(({ data }: { data: ResponseType }) => {
-                    if (data.code !== 2000)
-                        throw new Error(
-                            "An erorr occured while fetching room data.",
-                        );
-                    return data;
-                }),
-    });
 
     const staticMapURI = useStaticMapURI({
         apiKey: apiKey,
@@ -48,108 +53,107 @@ const Detail = () => {
         height: "300",
         markers: `markers=type:d|size:mid|color:red|pos:${data?.result.longitude}%20${data?.result.latitude}`,
     });
-    if (!isFetching && !isError)
-        return (
-            <>
-                <DefaultLayout>
-                    {olVisible && (
-                        <ImageDetail
-                            images={data?.result.imageUrls || []}
-                            closeEvent={() => setOlVisible(false)}
-                        />
-                    )}
-                    <TopSection
+
+    return (
+        <>
+            <DefaultLayout>
+                {olVisible && (
+                    <ImageDetail
                         images={data?.result.imageUrls || []}
-                        clickEvent={() => setOlVisible(true)}
+                        closeEvent={() => setOlVisible(false)}
                     />
-                    <MainContainer>
-                        <TitleSection
-                            title={data?.result.name || ""}
-                            location={data?.result.address || ""}
-                        />
-                        <InfoSection
-                            title="계약 정보"
-                            paragraphs={[
-                                {
-                                    title: "보증금",
-                                    content:
-                                        data?.result.deposit.toLocaleString(
-                                            "ko-KR",
-                                        ) + "만원",
-                                },
-                                {
-                                    title: "월세",
-                                    content:
-                                        data?.result.cost.toLocaleString(
-                                            "ko-KR",
-                                        ) + "만원",
-                                },
-                                {
-                                    title: "관리비",
-                                    content:
-                                        data?.result.maintenanceCost.toLocaleString(
-                                            "ko-KR",
-                                        ) + "원",
-                                },
-                                {
-                                    title: "계약기간",
-                                    content: data?.result.term,
-                                },
-                            ]}
-                        />
-                        <InfoSection
-                            title="방 정보"
-                            paragraphs={[
-                                {
-                                    title: "세대공동구역",
-                                    content: data?.result.commonArea,
-                                },
-                                {
-                                    title: "방 개수",
-                                    content: data?.result.type,
-                                },
-                                {
-                                    title: "전용 면적",
-                                    content: data?.result.exclusiveArea + "㎡",
-                                },
-                                {
-                                    title: "주차",
-                                    content: data?.result.parking
-                                        ? "가능"
-                                        : "불가능",
-                                },
-                                {
-                                    title: "난방방식",
-                                    content: data?.result.heatingSystem,
-                                },
-                            ]}
-                        />
-                        <OptionsSection
-                            title="옵션 정보"
-                            options={{
-                                furniture: data?.result.furniture,
-                                appliances: data?.result.appliances,
-                                prevention: data?.result.prevention,
-                                etc: data?.result.etc,
-                            }}
-                        />
-                        <DetailSection
-                            title="상세 정보"
-                            content={
-                                data?.result.detail || "No givien Information."
-                            }
-                        />
-                        <LocationSection
-                            title="위치"
-                            location={data?.result.address || "주소"}
-                        >
-                            <MapImage src={staticMapURI} />
-                        </LocationSection>
-                    </MainContainer>
-                    <BottomNavigation tel={data?.result.phone} />
-                </DefaultLayout>
-            </>
-        );
+                )}
+                <TopSection
+                    images={data?.result.imageUrls || []}
+                    clickEvent={() => setOlVisible(true)}
+                />
+                <MainContainer>
+                    <TitleSection
+                        title={data?.result.name || ""}
+                        location={data?.result.address || ""}
+                    />
+                    <InfoSection
+                        title="계약 정보"
+                        paragraphs={[
+                            {
+                                title: "보증금",
+                                content:
+                                    data?.result.deposit.toLocaleString(
+                                        "ko-KR",
+                                    ) + "만원",
+                            },
+                            {
+                                title: "월세",
+                                content:
+                                    data?.result.cost.toLocaleString("ko-KR") +
+                                    "만원",
+                            },
+                            {
+                                title: "관리비",
+                                content:
+                                    data?.result.maintenanceCost.toLocaleString(
+                                        "ko-KR",
+                                    ) + "원",
+                            },
+                            {
+                                title: "계약기간",
+                                content: data?.result.term,
+                            },
+                        ]}
+                    />
+                    <InfoSection
+                        title="방 정보"
+                        paragraphs={[
+                            {
+                                title: "세대공동구역",
+                                content: data?.result.commonArea,
+                            },
+                            {
+                                title: "방 개수",
+                                content: data?.result.type,
+                            },
+                            {
+                                title: "전용 면적",
+                                content: data?.result.exclusiveArea + "㎡",
+                            },
+                            {
+                                title: "주차",
+                                content: data?.result.parking
+                                    ? "가능"
+                                    : "불가능",
+                            },
+                            {
+                                title: "난방방식",
+                                content: data?.result.heatingSystem,
+                            },
+                        ]}
+                    />
+                    <OptionsSection
+                        title="옵션 정보"
+                        options={{
+                            furniture: data?.result.furniture,
+                            appliances: data?.result.appliances,
+                            prevention: data?.result.prevention,
+                            etc: data?.result.etc,
+                        }}
+                    />
+                    <DetailSection
+                        title="상세 정보"
+                        content={
+                            data?.result.detail || "No givien Information."
+                        }
+                    />
+                    <LocationSection
+                        title="위치"
+                        location={data?.result.address || "주소"}
+                    >
+                        <MapImage src={staticMapURI} />
+                    </LocationSection>
+                </MainContainer>
+                <BottomNavigation tel={data?.result.phone} />
+            </DefaultLayout>
+        </>
+    );
 };
 
 const MainContainer = styled.main`
