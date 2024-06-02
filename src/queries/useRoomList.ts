2a426dc7ab41a64ad8cb3849393cb9c9ef022ed4
용원 <auau3@kakao.com>
2024-06-02 12:23:55 +0900

    chore: useRoomList hook type 추가, ResponseType type 수정 - useRoomList: AxiosResponse 제네럴 타입 추가 - ResponseType: RoomDataType -> RoomDataType[]

--- src/queries/useRoomList.ts
+++ src/queries/useRoomList.ts
@@ -1,8 +1,10 @@
 import { mockApi } from "@/apis/mock";
+import { ResponseType } from "@/types/response";
 import { useQuery } from "@tanstack/react-query";
+import { AxiosResponse } from "axios";
 
 const useRoomList = (univName: string, deposit: number[], cost: number[]) => {
-    const { data, ...rest } = useQuery({
+    const { data, ...rest } = useQuery<AxiosResponse<ResponseType>>({
         queryKey: [`${new Date().getTime}.schoolId`],
         queryFn: () => mockApi.getRoomsBySchoolId(univName, deposit, cost),
     });
