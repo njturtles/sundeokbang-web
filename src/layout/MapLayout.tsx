15a5af3d7594f77c555ae68cca40bb60df406401
조용원 <auau3@kakao.com>
2024-06-09 17:36:44 +0900

    chore: background 클릭 시 FilterDetail 숨기기 기능 추가 - background overlay 클릭 시 FilterDetail 숨기는 기능 추가

--- src/layout/MapLayout.tsx
+++ src/layout/MapLayout.tsx
@@ -1,14 +1,14 @@
 import React, { CSSProperties, useRef } from "react";
 import styled from "@emotion/styled";
 import { FilterDetail } from "@/components/filter";
-import { useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import { overlayAtom } from "@/stores/overlay";
 import { AnimatePresence, motion } from "framer-motion";
 
 const variants = {
     animate: { y: 0 },
-    initial: { y: 500 },
-    exit: { y: 500 },
+    initial: { y: 470 },
+    exit: { y: 470 },
 };
 
 const MapLayout = ({
@@ -18,15 +18,20 @@ const MapLayout = ({
     children: React.ReactNode;
     style?: CSSProperties;
 }) => {
-    const overlay = useRecoilValue(overlayAtom);
-    const overlayRef = useRef<HTMLDivElement | null>(null);
+    const [overlay, setOverlay] = useRecoilState(overlayAtom);
+    const bottomSheetRef = useRef<HTMLDivElement | null>(null);
+
+    const bgClickHandler = (e: React.MouseEvent<HTMLDivElement>) => {
+        console.log(e);
+        if (e.target !== bottomSheetRef.current) setOverlay(false);
+    };
 
     return (
         <Container style={{ ...style }}>
             <AnimatePresence>
                 {overlay && (
                     <>
-                        <FilterContainer ref={overlayRef}>
+                        <FilterContainer ref={bottomSheetRef}>
                             <motion.div
                                 key="filter"
                                 variants={variants}
@@ -37,7 +42,7 @@ const MapLayout = ({
                                     y: { type: "spring", bounce: 0 },
                                     duration: 0.1,
                                 }}
-                                style={{ width: "100%", height: "500px" }}
+                                style={{ width: "100%", height: "470px" }}
                             >
                                 <FilterDetail />
                             </motion.div>
@@ -48,11 +53,12 @@ const MapLayout = ({
                             initial={{ opacity: 0 }}
                             exit={{ opacity: 0 }}
                             transition={{ duration: 0.3 }}
+                            onClick={bgClickHandler}
                             style={{
                                 position: "absolute",
                                 width: "100%",
                                 height: "100%",
-                                zIndex: "9999",
+                                zIndex: "999",
                                 backgroundColor: "rgba(0,0,0,0.1)",
                                 backdropFilter: "blur(5px)",
                             }}
@@ -75,12 +81,13 @@ const Container = styled.div`
 
 const FilterContainer = styled.div`
     position: absolute;
-    display: flex;
+    bottom: 0;
+    display: inline-flex;
     flex-direction: column;
     align-items: center;
     justify-content: flex-end;
     width: 100%;
-    height: 100%;
+    height: auto;
     z-index: 99999;
 `;
 
