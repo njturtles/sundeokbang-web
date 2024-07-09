4ff1692214ba623f1dce870465de8fcba567755a
조용원 <auau3@kakao.com>
2024-07-09 13:27:24 +0900

    feature: Authorization Interceptor 추가 - (added) JWT 방식 Authorization을 위한 인터셉터 추가 - (added) SSR일 시 별도의 토큰 기입을 통해 API Call 가능하도록 추가

--- src/apis/base.ts
+++ src/apis/base.ts
@@ -1,7 +1,27 @@
-import axios from "axios";
+import axios, { AxiosInstance, InternalAxiosRequestConfig } from "axios";
+import { getCookie } from "cookies-next";
 
 const baseURL = `${process.env.NEXT_PUBLIC_API_ENDPOINT}`;
 
 export const instance = () => {
     return axios.create({ baseURL: baseURL });
 };
+
+const withAuth = (instance: AxiosInstance) => {
+    if (typeof window !== "undefined") {
+        const userToken = getCookie("user");
+        instance.interceptors.request.use(
+            (config: InternalAxiosRequestConfig) => {
+                console.log("intercept");
+                config.headers["Authorization"] = userToken
+                    ? `Bearer ${userToken}`
+                    : "";
+                return config;
+            },
+        );
+    }
+
+    return instance;
+};
+
+export const authInstance = withAuth(instance());
--- src/apis/room.ts
+++ src/apis/room.ts
@@ -1,5 +1,22 @@
-import { instance } from "./base";
+import { authInstance } from "./base";
 
 export const roomApi = {
-    getRoomDetail: (roomId: string) => instance().get(`/rooms/${roomId}`),
+    getRoomDetail: (roomId: string, token?: string) =>
+        authInstance.get(`/rooms/${roomId}`, {
+            headers: token ? { Authorization: `Bearer ${token}` } : undefined,
+        }),
+    getRoomsBySchool: (
+        minDeposit: number,
+        maxDeposit: number,
+        minCost: number,
+        maxCost: number,
+    ) =>
+        authInstance.get(`/rooms`, {
+            params: {
+                minDeposit: minDeposit || undefined,
+                maxDeposit: maxDeposit || undefined,
+                minCost: minCost || undefined,
+                maxCost: maxCost || undefined,
+            },
+        }),
 };
