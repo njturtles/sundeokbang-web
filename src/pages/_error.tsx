c7bfbe0e9eec4bdbc1eada1e2f135d3ff3226b55
조용원 <auau3@kakao.com>
2024-07-18 13:57:18 +0900

    feature: error page 추가

---
+++ src/pages/_error.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "@emotion/styled";
+import LogoImage from "@/assets/images/logo_1.svg";
+
+const Error = () => {
+    return (
+        <Container>
+            <LogoImage />
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "24px",
+                }}
+            >
+                <Title>데이터를 가져오는 도중 에러가 발생하였습니다.</Title>
+            </div>
+        </Container>
+    );
+};
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 80px;
+    width: 500px;
+    height: 100vh;
+    margin: 0 auto;
+    padding: 0 20px;
+    font-size: 1.25rem;
+`;
+
+const Title = styled.h1`
+    font-size: 1.5rem;
+    font-weight: 500;
+    color: ${({ theme }) => theme.color.black.hue2};
+`;
+
+export default Error;
