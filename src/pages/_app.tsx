b488001414c9724487a527c7b2ed2e650f3564d2
조용원 <auau3@kakao.com>
2024-08-01 09:42:26 +0900

    fix: meta viewport 태그 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,11 @@ const App = ({ Component, pageProps }: AppProps) => {
         <>
             <Head>
                 <title>순덕방</title>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0, user-scalable=0"
+                />
+                <meta charSet="utf-8" />
             </Head>
             <QueryClientProvider client={qClient}>
                 <RecoilRoot>
